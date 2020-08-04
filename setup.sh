0fff943dffe81d1f0ca98af2276033bdd921a0ec
Kavan Mevada <kavanmevada@gmail.com>
2020-08-04 12:17:43 +0530

    Use pre existed cdns for css and js

--- setup.sh
+++ setup.sh
@@ -4,12 +4,3 @@ npm install
 # install gem dependencies
 gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
 bundle install
-
-# script for structuring the website
-echo 'structuring the website...'
-dependencies=('@fortawesome' 'bootstrap' 'jquery' 'moment' 'popper.js' 'slick-carousel')
-for dependency in "${dependencies[@]}"
-do
-    rsync -a node_modules/${dependency} assets
-    echo "Moved ${dependency} to assets/ folder"
-done
